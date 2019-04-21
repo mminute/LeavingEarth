a577d204ac5bf75f6626f40dd2b457fbc3656812
Mason Jennings <mjennings@pinterest.com>
2019-04-21 15:51:35 -0700

    Add Graph Class to represent game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import './App.css';
+import Graph from './Graph/Graph';
 
 import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
+    const gameBoard = new Graph({
+        'A': { 'B': 0, 'C': 0 },
+        'B': { 'A': 0, 'C': 1 },
+        'C': { 'A': 0, 'B': 1 },
+      });
+
+      window.console.log(gameBoard.getGraph());
+
     return (
       <div className="App">
         <header className="App-header">
